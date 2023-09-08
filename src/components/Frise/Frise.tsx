2bec7c0db49df0a2b4d7658633a96cc2faac70fa
Gautheron Olivia <oxtramag@gmail.com>
2023-09-08 19:56:30 +0200

    :bug: fix bug

---
+++ src/components/Frise/Frise.tsx
@@ -0,0 +1,17 @@
+import './Frise.css';
+
+function Frise() {
+  return (
+    <div className='Frise'>
+        <div className='Frise_rect'>
+            <span className='Frise_round_one'></span>
+            <span className='Frise_round_two'></span>
+            <span className='Frise_round_three'></span>
+            <span className='Frise_round_for'></span>
+            <span className='Frise_round_five'></span>
+        </div>
+    </div>
+  );
+}
+
+export default Frise;
