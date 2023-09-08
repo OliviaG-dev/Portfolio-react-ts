727befdca21c87610395c5bbdba769110e1b5f77
Gautheron Olivia <oxtramag@gmail.com>
2023-09-08 20:40:55 +0200

    :sparkles: feat:add component frise

--- src/components/Frise/Frise.tsx
+++ src/components/Frise/Frise.tsx
@@ -2,14 +2,17 @@ import './Frise.css';
 
 function Frise() {
   return (
-    <div className='Frise'>
-        <div className='Frise_rect'>
-            <span className='Frise_round_one'></span>
-            <span className='Frise_round_two'></span>
-            <span className='Frise_round_three'></span>
-            <span className='Frise_round_for'></span>
-            <span className='Frise_round_five'></span>
-        </div>
+    <div className="Frise">
+      <div className="rect_round">
+        <span className="Frise_round_one"></span>
+        <span className="Frise_round_two"></span>
+        <span className="Frise_round_three"></span>
+        <span className="Frise_round_for"></span>
+        <span className="Frise_round_five"></span>
+        {/* <span className="Frise_round_six"></span>
+        <span className="Frise_round_seven"></span> */}
+      </div>
+      <div className="Frise_rect"></div>
     </div>
   );
 }
