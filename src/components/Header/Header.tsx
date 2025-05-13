779268c696ea0204f22813e2b5b1b588db9443b9
Gautheron Olivia <oxtramag@gmail.com>
2025-05-13 15:38:39 +0200

    📱 feat:responsive header with burger menu

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,10 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './Header.css';
 import Logo from '../../assets/Logo.svg';
 
 function Header() {
   const [isScrolled, setIsScrolled] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const handleScroll = () => {
     if (window.scrollY > 0) {
@@ -13,21 +14,56 @@ function Header() {
     }
   };
 
-  window.addEventListener('scroll', handleScroll);
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
 
   return (
     <header className={isScrolled ? 'header_shrink' : ''}>
       <div className="header_container">
         <img className="header_img" src={Logo} alt="Logo de mon portfolio" />
-        <ul className="header_list">
+        <div
+          className={`burger_menu ${isMenuOpen ? 'active' : ''}`}
+          onClick={toggleMenu}
+        >
+          <div className="burger_line"></div>
+          <div className="burger_line"></div>
+          <div className="burger_line"></div>
+        </div>
+        <ul className={`header_list ${isMenuOpen ? 'active' : ''}`}>
           <li className="header_item">
-            <a className="header_link" href="#section_home">About</a>
+            <a className="header_link" href="#section_home" onClick={closeMenu}>
+              About
+            </a>
           </li>
           <li className="header_item">
-            <a className="header_link" href="#section_projects">Projets</a>
+            <a
+              className="header_link"
+              href="#section_projects"
+              onClick={closeMenu}
+            >
+              Projets
+            </a>
           </li>
           <li className="header_item">
-            <a className="header_link" href="#section_contact">Contact</a>
+            <a
+              className="header_link"
+              href="#section_contact"
+              onClick={closeMenu}
+            >
+              Contact
+            </a>
           </li>
         </ul>
       </div>
